4a567076d10901e3bac4841eb063f1088f59bdb3
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-06 01:19:20 +0200

    add empty project for firdy problem + useful link

---
+++ 01_Shapes/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Shapes
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+//Microsoft Docs ('virtual' methods & references + example) ->  https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/virtual
