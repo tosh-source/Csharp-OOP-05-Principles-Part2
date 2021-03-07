caafffda420f137bc395a2df0a0d9a252c24b4c6
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-07 20:58:06 +0200

    Add tests and complete problem 1 - Shapes.

--- 01_Shapes/Tests.cs
+++ 01_Shapes/Tests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _01_Shapes.Models;
 
 namespace _01_Shapes
 {
@@ -10,6 +11,17 @@ namespace _01_Shapes
     {
         static void Main(string[] args)
         {
+            Shape[] shapes = 
+            {
+                new Square(18),       //S = 324
+                new Rectangle(9, 4),  //S = 36
+                new Triangle(9, 4)    //S = 18
+            };
+
+            foreach (var currentShape in shapes)
+            {
+                Console.WriteLine(currentShape.CalculateSurface());
+            }
         }
     }
 }
