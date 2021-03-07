a5fbec8c56bf65f18da3c27d4babfb55c1b251b1
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-07 20:34:27 +0200

    Complete implementation of Square class.

---
+++ 01_Shapes/Models/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Shapes.Models
+{
+    class Square : Shape
+    {
+        public Square(double height)
+        {
+            base.Height = height;
+        }
+
+        public override double CalculateSurface()
+        {
+            return Height * Height;
+        }
+    }
+}
