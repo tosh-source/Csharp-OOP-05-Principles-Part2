853a841e6e873bc779878ae5569e9e737a234c63
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-17 00:54:57 +0200

    add BankProduct class

---
+++ 02_BankAccounts/Bank/BankProducts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02_BankAccounts.Bank.Accounts;
+using _02_BankAccounts.Bank.Customers;
+
+namespace _02_BankAccounts.Bank
+{
+    class BankProducts
+    {
+        private Deposit deposit;
+        private Loan loan;
+        private Mortgage mortgage;
+
+        public Deposit Deposit
+        {
+            get { return deposit; }
+            set { deposit = value; }
+        }
+
+        public Loan Loan
+        {
+            get { return loan; }
+            set { loan = value; }
+        }
+
+        public Mortgage Mortgage
+        {
+            get { return mortgage; }
+            set { mortgage = value; }
+        }
+
+        public BankProducts()
+        {
+
+        }
+    }
+}
