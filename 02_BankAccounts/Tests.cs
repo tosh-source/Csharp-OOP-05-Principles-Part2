06f31fd306a5d1f423a93ade01064b568e84da0f
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-24 14:12:54 +0200

    add mmore tests

--- 02_BankAccounts/Tests.cs
+++ 02_BankAccounts/Tests.cs
@@ -12,18 +12,36 @@ namespace _02_BankAccounts
     {
         static void Main(string[] args)
         {
+            //I.Add individual bank client.
             BankProducts johnsAccounts = new BankProducts();
 
-            //Deposit some money
+            //Try to withdraw money from empty deposit account.
             johnsAccounts.Deposit.WithdrawMoney(246);
             //You can not get money from an empty account! So for that reason you need to use correct method "Deposit.DepositMoney()".
+            
+            //Deposit some money
             johnsAccounts.Deposit.DepositMoney(246);
             Console.WriteLine("Deposit balance: " + johnsAccounts.Deposit.Balance);
 
             johnsAccounts.Deposit.Customer.Type = CustomerType.Individuals.ToString();
             johnsAccounts.Deposit.Customer.Name = "John Johns";
 
+            //II.Add company bank client
+            var company = new Companies();
+            BankProducts companyAccount = new BankProducts(company);
 
+            //Deposit some money
+            companyAccount.Deposit.DepositMoney(2500);
+            companyAccount.Mortgage.DepositMoney(6000);
+            companyAccount.Loan.DepositMoney(300);
+
+            //Calculate interest rate for company's mortgage account.
+            companyAccount.Mortgage.CalculateInterestRate(12, 6);
+            Console.WriteLine("Company mortgage interest: " + companyAccount.Mortgage.Interest);
+
+            //Calculate interest rate for company's loan account.
+            companyAccount.Loan.CalculateInterestRate(3, 8);
+            Console.WriteLine("Company loan account interest: " + companyAccount.Loan.Interest);
         }
     }
 }
