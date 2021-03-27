5c6ba25c0016ced3fdaf13991d20d7b69947ae25
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-27 02:32:49 +0200

    Work on problem 3. DateTime check completed.

---
+++ 03_RangeException/Models/InvalidRangeException.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_RangeException.Models
+{
+    class InvalidRangeException<T>
+    {
+        public string Start { get; private set; }
+        public string End { get; private set; }
+        public string ErrorMessage { get; private set; }
+
+        public InvalidRangeException(int start, int end) 
+            : this(start.ToString(), end.ToString())
+        {
+        }
+        public InvalidRangeException(string start, string end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public void CheckRange(T objectToCompare)
+        {
+            if (objectToCompare is DateTime)
+            {
+                try
+                {
+                    DateTime tempDateTime = DateTime.Parse(objectToCompare.ToString());
+                    DateTime startDateTime = DateTime.Parse(this.Start);
+                    DateTime endDateTime = DateTime.Parse(this.End);
+
+                    if (tempDateTime < startDateTime || tempDateTime > endDateTime)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    else
+                    {
+                        this.ErrorMessage = "Everything in range!";
+                    }
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    this.ErrorMessage = ex.Message;
+                }
+                catch (Exception ex)
+                {
+                    this.ErrorMessage += ex.Message;
+                    throw;
+                }
+            }
+            else if (objectToCompare is int)
+            {
+
+            }
+            else if (objectToCompare is string)
+            {
+
+            }
+        }
+    }
+}
---
+++ 03_RangeException/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using _03_RangeException.Models;
+using System.Threading;
+
+namespace _03_RangeException
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture; 
+
+            //Check DateTime
+            var dateTimeRangeException = new InvalidRangeException<DateTime>("1.1.1980", "12.31.2013");
+            var dateTime = DateTime.Parse("12.31.2013");
+            dateTimeRangeException.CheckRange(dateTime);
+            Console.WriteLine(dateTimeRangeException.ErrorMessage);
+
+            dateTime = DateTime.Parse("12.31.2020");
+            dateTimeRangeException.CheckRange(dateTime);
+            Console.WriteLine(dateTimeRangeException.ErrorMessage);
+
+        }
+    }
+}
